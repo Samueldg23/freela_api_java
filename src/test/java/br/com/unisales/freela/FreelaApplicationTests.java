053d1c104578d1c3991b91f0105da981859ffffc
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-07 19:44:34 -0300

    first commit

---
+++ src/test/java/br/com/unisales/freela/FreelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisales.freela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
