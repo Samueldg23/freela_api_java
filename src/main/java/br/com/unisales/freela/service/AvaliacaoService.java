7f7458a1c14d2db145d46b295b4c8a14949c1145
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-11 20:11:19 -0300

    ajustes na avaliaçãoservice

---
+++ src/main/java/br/com/unisales/freela/service/AvaliacaoService.java
@@ -0,0 +1,35 @@
+package br.com.unisales.freela.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.unisales.freela.dto.AvaliacaoDTO;
+import br.com.unisales.freela.model.Avaliacao;
+import br.com.unisales.freela.repository.AvaliacaoRepository;
+
+@Service
+public class AvaliacaoService {
+
+    @Autowired
+    public AvaliacaoRepository repo;
+
+    public AvaliacaoDTO cadastrarAvaliacao (AvaliacaoDTO avaliacao) {
+        Avaliacao avaliacaoNovo = new Avaliacao();
+        avaliacaoNovo.setComentario(avaliacao.getComentario());
+        avaliacaoNovo.setNota(avaliacao.getNota());
+        avaliacaoNovo.setPedido(null);       
+
+        avaliacaoNovo = repo.save(avaliacaoNovo);
+        return converterParaDTO(avaliacaoNovo);
+    }
+    public AvaliacaoDTO converterParaDTO(AvaliacaoDTO avaliacao) {
+        AvaliacaoDTO avaliacaoDTO = new AvaliacaoDTO();
+        avaliacaoDTO.setId(avaliacao.getId());
+        avaliacaoDTO.setId_avaliador(avaliacao.getId_avaliador());
+        avaliacaoDTO.setId_avaliado(avaliacao.getId_avaliado());
+        avaliacaoDTO.setNota(avaliacao.getNota());
+        avaliacaoDTO.setComentario(avaliacao.getComentario());
+
+        return avaliacaoDTO;
+    }
+}
